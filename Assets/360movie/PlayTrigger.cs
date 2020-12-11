e843acd70176e36395c384181ae5066277e85344
TUmedu <wewe53540@gmail.com>
2020-12-11 23:47:30 +0900

    walks

---
+++ Assets/360movie/PlayTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayTrigger : MonoBehaviour
+{
+   
+
+    public VideoClip videoClip;
+    //public GameObject screen;
+
+    void Start()
+    {
+        //var videoPlayer = screen.AddComponent<VideoPlayer>();   // videoPlayeコンポーネントの追加
+
+        //videoPlayer.source = VideoSource.VideoClip; // 動画ソースの設定
+        //videoPlayer.clip = videoClip;
+
+        //videoPlayer.isLooping = true;   // ループの設定
+    }
+
+    public void VPControl()
+    {
+        var videoPlayer = GetComponent<VideoPlayer>();
+
+        if (!videoPlayer.isPlaying) // ボタンを押した時の処理
+            videoPlayer.Play(); // 動画を再生する。
+        else
+            videoPlayer.Pause();    // 動画を一時停止する。
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var videoPlayer = GetComponent<VideoPlayer>();
+
+        Debug.Log("walk！");
+        videoPlayer.Play(); // 動画を再生する。
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        var videoPlayer = GetComponent<VideoPlayer>();
+
+        Debug.Log("通り抜け終えた");
+        videoPlayer.Pause();    // 動画を一時停止する。
+    }
+}
